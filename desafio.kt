09628a0bd6232253fddd5533e91a579486f9d389
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2023-01-02 20:39:45 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,16 +1,16 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL } // ainda n está sendo usado
 
-class Usuario
-
+class Usuario //poderia virar uma data class
+                                //val talvez seja melhor que var ou vice versa
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
+                    //varargs pode ser usado para adicionar multiplos usuarios
+    fun matricular(usuario: Usuario) { //quando a função matricular for chamada, o objetivo é que usuario seja adiciojnado na lista de inscritos
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
@@ -19,3 +19,5 @@ fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
+
+//substituir o codigo quando terminar
