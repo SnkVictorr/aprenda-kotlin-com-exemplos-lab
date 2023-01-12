fd5b226668d03e78e9550c0f287ea2cd6904d20e
SnkVictorr <99307258+SnkVictorr@users.noreply.github.com>
2023-01-12 20:46:52 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,23 +1,46 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL } // ainda n está sendo usado
+data class Usuarios(val nome: String, val idade: Int)
 
-class Usuario //poderia virar uma data class
-                                //val talvez seja melhor que var ou vice versa
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-                    //varargs pode ser usado para adicionar multiplos usuarios
-    fun matricular(usuario: Usuario) { //quando a função matricular for chamada, o objetivo é que usuario seja adiciojnado na lista de inscritos
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+  	val inscritos1 = mutableListOf<Usuarios>()
+  	val inscritos2 = mutableListOf<Usuarios>()
+  
+    fun matricular1(usuario: Usuarios) {
+        inscritos1.add(usuario)
+        
+        
+    }
+    fun matricular2(usuario: Usuarios) {
+        inscritos2.add(usuario)
+        
+    }
+    fun print1() {
+        println("$inscritos1 matriculados no ${conteudos[0]}")
+    }
+	fun print2() {
+        println("$inscritos2 matriculado no ${conteudos[1]}")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+    val user1 = Usuarios("Marcos", 25)
+	  val user2 = Usuarios("João", 35)
+    val user3 = Usuarios("Ademir", 18)
 
-//substituir o codigo quando terminar
+    
+    val conteudo1 = ConteudoEducacional("Kotlin Experience", 40, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Formação Power BI Analyst", 60, nivel = Nivel.INTERMEDIARIO)
+
+    val formacao1 = Formacao("$conteudo1", listOf(conteudo1, conteudo2))
+    
+    formacao1.matricular1(user1)
+    formacao1.matricular1(user2)
+    formacao1.matricular2(user3)
+    
+	  formacao1.print1()
+    formacao1.print2()
+}
